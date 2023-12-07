792e2f6c3c31a4e2fa637b6e9eb320037f95bd27
Engr Ugwu <fugwungene@gmail.com>
2023-12-07 22:13:59 +0000

    Fix code style error

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,14 +7,14 @@ sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 fake_file=" /data/web_static/releases/test/index.html"
-echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolbertonSchool\n\t</body>\n<\html>" | sudo tee $fake_file > /dev/null
+echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolbertonSchool\n\t</body>\n<\html>" | sudo tee "$fake_file" > /dev/null
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 
 config_file=/etc/nginx/sites-available/default
-sed -i '55a \ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' $config_file
+sed -i '55a \ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' "$config_file"
 
 sudo nginx -t
 sudo service nginx restart
