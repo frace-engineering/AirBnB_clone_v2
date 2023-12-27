7c0e7d3b1b988febfbaee8f6f922412846f19391
Engr Ugwu <fugwungene@gmail.com>
2023-12-27 22:26:58 +0000

    Code task 3
    Implemet optional subdirectory

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Python script that starts a web application """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """ Display "Hello HBNB!" on the browser"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Display "HBNB!" on the browser"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """ Display "C {text}" on the browser"""
+    text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+@app.route("/python/", strict_slashes=False)
+@app.route("/python", strict_slashes=False)
+def python_text(text='is cool'):
+    """ Display "Python {text}" on the browser"""
+    text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+if __name__ == "__main__":
+    """ Run the app on port 5000 and all host """
+    app.run(host='0.0.0.0', port=5000)
