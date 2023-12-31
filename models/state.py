c3c3e83ac8319dd456f49c4fee96327468bbd937
Engr Ugwu <fugwungene@gmail.com>
2023-12-31 15:15:14 +0000

    Modify some files

--- models/state.py
+++ models/state.py
@@ -3,22 +3,23 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.city import City
 from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'state'
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state',
+        cities = relationship('City', back_populates='state_rel',
                               cascade='all, delete-orphan')
     else:
         @property
         def cities(self):
             """property getter for the cities"""
             from models import storage
-            city_objs = storage.all("City").values()
+            city_objs = storage.all('City').values()
             return [city for city in city_objs if
                     city.state_id == self.id]
