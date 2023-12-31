c2fd224d501a19ea886bc371a99afa1c729b7758
Engr Ugwu <fugwungene@gmail.com>
2023-12-31 16:00:02 +0000

    Fix code style issue

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,14 @@ from sqlalchemy import Column, String, DateTime
 
 
 Base = declarative_base()
+
+
 class BaseModel:
     """A base class for all hbnb models"""
-    id = Column(String(60), primary_key=True, nullable=False,unique=True)
+    id = Column(String(60), primary_key=True, nullable=False, unique=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
